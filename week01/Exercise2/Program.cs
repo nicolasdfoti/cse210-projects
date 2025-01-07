8560a37a9e67bdcb25469b4b161382eb0dbc7999
nicolasdfoti <nicolasdfoti@gmail.com>
2025-01-06 22:02:03 -0300

    finishing ex 2

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -5,5 +5,35 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise2 Project.");
+
+        Console.WriteLine("What is your grade percentage? ");
+        string gradeAnswer = Console.ReadLine();
+        int grade = int.Parse(gradeAnswer);
+
+        if (grade >= 90)
+            {
+                Console.WriteLine("You've got an A");
+            }
+
+        else if (grade > 80 && grade < 90)
+            {
+                Console.WriteLine("You've got a B.");
+            }
+
+        else if (grade > 70 && grade < 80)
+            {
+                Console.WriteLine("You've got a C.");
+            }
+
+        else if (grade > 60 && grade <= 70)
+            {
+                Console.WriteLine("You've got a D.");
+            }
+        
+        else if (grade < 60)
+            {
+                Console.WriteLine("You've got an F.");
+            }
+
     }
 }
