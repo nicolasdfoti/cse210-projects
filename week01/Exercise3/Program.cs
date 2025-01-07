1edbfc806fbd9fc19beee76aae585bdf87942a84
nicolasdfoti <nicolasdfoti@gmail.com>
2025-01-07 12:42:42 -0300

    Completing Ex 3

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -5,5 +5,20 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise3 Project.");
+
+        int randomNumber = 6
+
+        do
+        {
+            Console.WriteLine("What is your guess? ");
+            string answer = Console.ReadLine();
+            int answer = int.Parse(answer);
+        } while (answer != randomNumber);
+
+        if (answer == randomNumber)
+            {
+                Console.WriteLine($"Congrats! {randomNumber} was the correct answer!")
+            }
+
     }
 }
