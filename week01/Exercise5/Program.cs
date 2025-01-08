f89ce02a00ddfc50041683802d3d6eca7a1bf66c
nicolasdfoti <nicolasdfoti@gmail.com>
2025-01-07 22:47:14 -0300

    Added the functions

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -6,4 +6,35 @@ class Program
     {
         Console.WriteLine("Hello World! This is the Exercise5 Project.");
     }
+
+    static void DisplayWelcome()
+        {
+            Console.WriteLine("Welcome to the Program!");
+        }
+
+        static string PromptUserName()
+        {
+            Console.WriteLine("What's your first name? ");
+            string firstName = Console.ReadLine();
+
+            return firstName;
+        }
+
+        static int PromptUserNumber()
+        {
+            Console.WriteLine("What is your favorite number? ");
+            int favoriteNumber = Console.ReadLine();
+
+            return favoriteNumber;
+        }
+
+        static int SquareNumber(int number)
+        {
+            int squared = number * number
+        }
+
+        static string DisplayResult(string firstName, int squared)
+        {
+            Console.WriteLine($"{firstName}, the square of your number is {squared}");
+        }
 }
