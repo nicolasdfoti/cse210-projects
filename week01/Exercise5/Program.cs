cd5665171d104715007cae74551c890e47808c0a
nicolasdfoti <nicolasdfoti@gmail.com>
2025-01-07 22:56:28 -0300

    Finishing!

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -5,6 +5,13 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise5 Project.");
+
+        DisplayWelcome();
+        string name = PromptUserName();
+        int number = PromptUserNumber();
+        int squared = SquareNumber(number);
+        DisplayResult(name, squared);
+
     }
 
     static void DisplayWelcome()
@@ -23,17 +30,20 @@ class Program
         static int PromptUserNumber()
         {
             Console.WriteLine("What is your favorite number? ");
-            int favoriteNumber = Console.ReadLine();
+            string number = Console.ReadLine();
+            int favoriteNumber = int.Parse(number);
 
             return favoriteNumber;
         }
 
         static int SquareNumber(int number)
         {
-            int squared = number * number
+            int squared = number * number;
+
+            return squared;
         }
 
-        static string DisplayResult(string firstName, int squared)
+        static void DisplayResult(string firstName, int squared)
         {
             Console.WriteLine($"{firstName}, the square of your number is {squared}");
         }
