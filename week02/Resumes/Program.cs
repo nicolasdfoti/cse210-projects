70a16091c524248f0b399b6e770091d547b89790
Nicolás Foti <nicolasdfoti@gmail.com>
2025-01-11 19:07:23 -0300

    Practiced in SandBox and started Resume Exercise

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -6,4 +6,24 @@ class Program
     {
         Console.WriteLine("Hello World! This is the Resumes Project.");
     }
+
+           public class Job
+    {
+        string _company = "";
+        string _job_title = "";
+        int _start_year = 0;
+        int _end_year = 0;
+
+        public void JobInformation()
+        {
+            Console.WriteLine($"{_job_title} ({_company}) {_start_year} - {_end_year}");
+        }
+
+    }
+
+        public class Resume
+        {
+            string _name = "";
+            List<Job> _jobs = 
+        }
 }
