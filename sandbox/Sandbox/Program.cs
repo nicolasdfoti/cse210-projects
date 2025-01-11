92cd8289676f883dee3f70710647fa15dde18cde
nicolasdfoti <nicolasdfoti@gmail.com>
2025-01-11 10:31:08 -0300

    Played with sandbox

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -5,5 +5,34 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Sandbox Project.");
+
+        Player player = new Player();
+
+        Console.Write("Which player are you entering? ");
+        player._name = Console.ReadLine();
+
+        Console.Write("Enter the surname: ");
+        player._surname = Console.ReadLine();
+
+        Console.Write("Enter the team: ");
+        player._team = Console.ReadLine();
+
+        player.Presentation();
     }
+
+    public class Player
+    {
+        public string _name = "";
+        public string _surname = "";
+        public string _team = "";
+
+        public void Presentation()
+        {
+        Console.WriteLine($"{_name}, {_surname}, {_team}");
+        }
+
+
+    }
+
+    
 }
