d9b14706248407ff7d5e5a4c3a700230b2021595
Nicolás Foti <nicolasdfoti@gmail.com>
2025-01-14 22:16:01 -0300

    Started Journal Project

--- week02/Journal/Program.cs
+++ week02/Journal/Program.cs
@@ -5,5 +5,36 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Journal Project.");
+
+        string answer = "";
+
+        while (answer != "5") {
+            Console.WriteLine("1. Add event");
+            Console.WriteLine("2. Display Journal");
+            Console.WriteLine("3. Save to a file");
+            Console.WriteLine("4. Load from a file");
+            Console.WriteLine("5. Exit");
+        }
+
+        if (answer == "1") {
+
+        }
+
+        else if (answer == "2") {
+
+        }
+
+        else if (answer == "3") {
+
+        }
+
+        else if (answer == "4") {
+
+        }
+
+        else if (answer == "5") {
+            Console.WriteLine("Thanks for using the Journal App! Goodbye!");
+        }
+
     }
 }
