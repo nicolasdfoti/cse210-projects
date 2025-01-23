a3924994299397c8a83c30a68cb6e3bc079950a9
nicolasdfoti <nicolasdfoti@gmail.com>
2025-01-23 15:32:17 -0300

    Added creativity by creating a list of scriptures with references

--- week03/ScriptureMemorizer/Program.cs
+++ week03/ScriptureMemorizer/Program.cs
@@ -7,12 +7,19 @@ class Program
         Console.WriteLine("Hello World! This is the ScriptureMemorizer Project.");
         Console.WriteLine();
 
-        Reference reference = new Reference("Joshua", 1, 9 );
+        List<Scripture> scriptures = new List<Scripture>() {
+            
+            new Scripture(new Reference("Joshua", 1, 9), "Have not I commanded thee? Be strong and of a good courage; be not afraid, neither be thou dismayed: for the Lord thy God is with thee whithersoever thou goest."),
+            new Scripture(new Reference("3 Nephi", 11, 9, 10), "And it came to pass that he stretched forth his hand and spake unto the people, saying: Behold, I am Jesus Christ, whom the prophets testified shall come into the world."),
+            new Scripture(new Reference("Mosiah", 3, 19), "For the natural man is an enemy to God, and has been from the fall of Adam, and will be, forever and ever, unless he yields to the enticings of the Holy Spirit, and putteth off the natural man and becometh a saint through the atonement of Christ the Lord, and becometh as a child, submissive, meek, humble, patient, full of love, willing to submit to all things which the Lord seeth fit to inflict upon him, even as a child doth submit to his father."),
 
-        string text = "Have not I commanded thee? Be strong and of a good courage; be not afraid, neither be thou dismayed: for the Lord thy God is with thee whithersoever thou goest.";
+        };
 
-        Scripture scripture = new Scripture(reference, text);
-        Console.WriteLine(scripture.GetDisplayText());
+        Random random = new Random();
+        int randomIndex = random.Next(scriptures.Count);
+        Scripture selectedScripture = scriptures[randomIndex];
+
+        Console.WriteLine(selectedScripture.GetDisplayText());
         Console.WriteLine();
 
         Console.WriteLine("Press ENTER to continue. Type QUIT to finish");
@@ -22,12 +29,12 @@ class Program
 
             if (choice == "") {
 
-                scripture.HideRandomWords(3);
+                selectedScripture.HideRandomWords(1);
 
-                Console.WriteLine(scripture.GetDisplayText());
+                Console.WriteLine(selectedScripture.GetDisplayText());
                 Console.WriteLine();
 
-                if (scripture.IsCompletelyHidden()) {
+                if (selectedScripture.IsCompletelyHidden()) {
                     break;
                 }
 
