1bbfeac28134b63fbddbb38727225ba4af42351c
nicolasdfoti <nicolasdfoti@gmail.com>
2025-01-27 12:30:56 -0300

    Created YoutubeVideos classes and attributes

---
+++ week04/YouTubeVideos/Comment.cs
@@ -0,0 +1,5 @@
+class Comment
+{
+    public string _person;
+    public string comment;
+}
---
+++ week04/YouTubeVideos/Video.cs
@@ -0,0 +1,7 @@
+class Video 
+{
+    public string _title;
+    public string _author;
+    public int _length;
+    public List<Comment> _comments = new List<Comment>();
+}
