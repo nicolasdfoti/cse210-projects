3e275bbc6775aef9a85c94c576a23dd877030e3d
nicolasdfoti <nicolasdfoti@gmail.com>
2025-02-03 13:28:45 -0300

    Addede the classes and Program basic functions

--- week05/Mindfulness/Program.cs
+++ week05/Mindfulness/Program.cs
@@ -5,5 +5,38 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Mindfulness Project.");
+        Console.WriteLine();
+
+        Console.WriteLine("Welcome to the Mindfulness Program!");
+        Console.WriteLine("Choose one of the following: ");
+        Console.WriteLine();
+        Console.WriteLine("1. Breathing Activity.");
+        Console.WriteLine("2. Reflecting Activity.");
+        Console.WriteLine("3. Listing Activity.");
+
+        string option = Console.ReadLine();
+
+        while (option != "quit".ToLower()) {
+
+            if (option == "1") {
+
+            }
+
+            else if (option == "2") {
+
+            }
+
+            else if (option == "3") {
+
+            }
+
+            else {
+                Console.WriteLine("Please, choose a valid answer in numbers");
+            }
+
+        }
+
+        Console.WriteLine("Thanks! Have a great day!");
+
     }
 }
