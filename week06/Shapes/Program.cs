054589e1e1b0de1e0af499e8f5e64d1cb06d6abb
nicolasdfoti <nicolasdfoti@gmail.com>
2025-02-10 17:41:30 -0300

    Finished Shapes exercise

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -5,5 +5,15 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Shapes Project.");
+        Console.WriteLine();
+
+        Square square = new Square(4, "red");
+        Console.WriteLine($"The square has an area of {square.GetArea()} and it's {square.GetColor()}");
+
+        Rectangle rectangle = new Rectangle(30, 20, "green");
+        Console.WriteLine($"The rectangle has an area of {rectangle.GetArea()} and it's {rectangle.GetColor()}");
+
+        Circle circle = new Circle(10, "brown");
+        Console.WriteLine($"The circle has an area of {circle.GetArea()} and it's {circle.GetColor()}");
     }
 }
